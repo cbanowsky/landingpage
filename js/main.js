6e9580f8b4b6d953b9f4cc542d5749ca8dbdce6a
Cameron Banowsky <cameron@hack.cd>
2017-11-01 20:47:47 -0700

    v.1.0

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ $(function () {
+  $("#datepicker").datepicker({
+        autoclose: true,
+        todayHighlight: true
+  }).datepicker('update', new Date());;
+});
+
+});
